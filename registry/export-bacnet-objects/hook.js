42f4edb8b22b3e9e54d2581fcbea103d6a0c3d58
Yevhenii Matviichuk <yevhenii.matviichuk@gmail.com>
2025-08-20 10:55:45 -0400

    Export bacnet object initial implementation

--- registry/export-bacnet-objects/hook.js
+++ registry/export-bacnet-objects/hook.js
@@ -5,6 +5,28 @@ const NormalSdk = require("@normalframework/applications-sdk");
  * @param {NormalSdk.InvokeParams} params
  * @returns {NormalSdk.InvokeResult}
  */
-module.exports = async ({ points }) => {
-  console.log(points.length);
+module.exports = async ({ points, sdk }) => {
+  for (const point of points) {
+    try {
+      // the point is created by the global variable, but automatically export this as BACnet
+      await sdk.http.post("/api/v1/bacnet/local", {
+        "uuid": point.uuid,
+        "object_id": {
+          "object_type": "OBJECT_BINARY_VALUE"
+        },
+        "props": [
+          {
+            "property": "PROP_OBJECT_NAME",
+            "value": {
+              "character_string": "Heartbeat"
+            }
+          }
+        ]
+      })
+    } catch (e) {
+      if (e.status !== 409) {
+        console.error("BACnet POST error:", e.message || e);
+      }
+    }
+  }
 };
